fc523aa7b1b6bd3f3ff8c31b6fd587add81d75f3
Michal Wrobel <sparrovv@gmail.com>
2014-09-06 10:54:41 +0100

    Update gotr to v0.4.0

--- gotr.rb
+++ gotr.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Gotr < Formula
   homepage 'https://github.com/sparrovv/gotr'
-  url 'https://github.com/sparrovv/gotr/releases/download/v0.3.0/gotr-v0.3.0.tar.gz'
-  sha1 "deecd7cf7af0eb0a826066c3fdc49a7eb0b069ee"
+  url 'https://github.com/sparrovv/gotr/releases/download/v0.4.0/gotr-v0.4.0.tar.gz'
+  sha1 "10f2558fbd6b305a643db70ab05a35b4cf09577c"
 
   depends_on :arch => :x86_64
 

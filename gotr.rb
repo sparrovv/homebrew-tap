56ccc8aee48cbb5fff714984b6bc99053928a1c8
Michal Wrobel <sparrovv@gmail.com>
2015-04-08 20:36:43 +0100

    Update the whole URL

--- gotr.rb
+++ gotr.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Gotr < Formula
   homepage 'https://github.com/sparrovv/gotr'
-  url 'https://github.com/sparrovv/gotr/releases/download/v0.4.0/gotr-v0.4.1.tar.gz'
+  url 'https://github.com/sparrovv/gotr/releases/download/v0.4.1/gotr-v0.4.1.tar.gz'
   sha1 "2eacd99ca153cf1c3dbb87af25196112861dc486"
 
   depends_on :arch => :x86_64
